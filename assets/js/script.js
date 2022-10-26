0b99ec1f7df6f04282196a82b6cfc495470089a3
GaborFicsor <ficsorgabor@gmail.com>
2022-10-26 16:53:00 +0000

    Add global variables to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const userScore = 0;
+const computerScore = 0;
+const userScoreSpan = document.getElementById("user-score");
+const computerScoreSpan = document.getElementById("computer-score");
+const scoreBoardDiv = document.querySelector(".score-board");
+const resultDiv = document.querySelector(".message");
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const lizard = document.getElementById("lizard");
+const spock = document.getElementById("spock");

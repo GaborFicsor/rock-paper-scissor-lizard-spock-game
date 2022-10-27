bbb0a3304ffb9e1164d86ba4e306e79b28af0681
GaborFicsor <ficsorgabor@gmail.com>
2022-10-27 15:09:34 +0000

    Add switch statement to game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,10 +20,44 @@ function getComputerChoice() {
 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
-    console.log("user choice is " + userChoice);
-    console.log("computer choice is " + computerChoice);
-
+    switch (userChoice + computerChoice) {
+        case "rockscissors":
+        case "rocklizard":
+        case "paperrock":
+        case "paperspock":
+        case "scissorspaper":
+        case "scissorslizard":
+        case "lizardspock":
+        case "lizardpaper":
+        case "spockscissors":
+        case "spockrock":
+            console.log("USER WINS");
+            break;
+        
+        case "rockspock":
+        case "rockpaper":
+        case "paperlizard":
+        case "paperscissors":
+        case "scissorsrock":
+        case "scissorsspock":
+        case "lizardscissors":
+        case "lizardrock":
+        case "spocklizard":
+        case "spockpaper":
+            console.log("USER LOSES");
+            break;
+        
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+        case "spockspock":
+        case "lizardlizard":
+            console.log("ITS A TIE");
+            break;
+        
+    }
 }
+
 
 
 
